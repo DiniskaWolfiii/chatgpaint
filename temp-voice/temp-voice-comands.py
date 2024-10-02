fe95ef2fbe2e73d731b086e2e5905c87536b17c2
Wolfiii <54332169+DiniskaWolfiii@users.noreply.github.com>
2024-10-02 13:28:23 +0200

    temp voice interface now available everywhere

--- temp-voice/temp-voice-comands.py
+++ temp-voice/temp-voice-comands.py
@@ -45,7 +45,7 @@ class TempVoiceCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.slash_command(name="temp-voice-interface", guild_ids=[1056514064081231872], description="Sends the message for the voicechannel Interface (Only Bot Owner)") # Pfotenclub Server
+    @commands.slash_command(name="temp-voice-interface", description="Sends the message for the voicechannel Interface (Only Bot Owner)") # Pfotenclub Server
     #@commands.slash_command(name="temp-voice-interface", guild_ids=[1001916230069911703], description="Sendet die Nachricht für das Sprachkanal Interface. (Nur für Bot Owner)") # Wolfiiis Server
     async def rolebutton(self, ctx: discord.ApplicationContext): # when the command is called
         if ctx.author.id != 327880195476422656: # if the author is not the bot owner
